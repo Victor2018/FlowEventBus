d5ad4a7ad7db758231b7abe3f6f08379208ff5a0
zhanghaifeng <zhanghaifeng@taodaji.com.cn>
2025-12-02 09:57:43 +0800

    add java api

--- library/src/main/java/com/cherry/library/floweventbus/post/PostEvent.kt
+++ library/src/main/java/com/cherry/library/floweventbus/post/PostEvent.kt
@@ -20,3 +20,15 @@ inline fun <reified T> postEvent(scope: ViewModelStoreOwner, event: T, timeMilli
     ViewModelProvider(scope).get(EventBusCore::class.java)
         .postEvent(T::class.java.name, event!!, timeMillis)
 }
+
+// Java API (非内联版本)
+fun <T> postEvent(event: T, clazz: Class<T>, timeMillis: Long = 0L) {
+    ApplicationScopeViewModelProvider.getApplicationScopeViewModel(EventBusCore::class.java)
+        .postEvent(clazz.name, event!!, timeMillis)
+}
+
+// 限定范围的事件 - Java API(非内联版本)
+fun <T> postEvent(scope: ViewModelStoreOwner, event: T, clazz: Class<T>, timeMillis: Long = 0L) {
+    ViewModelProvider(scope).get(EventBusCore::class.java)
+        .postEvent(clazz.name, event!!, timeMillis)
+}
